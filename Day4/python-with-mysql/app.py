d324875be26fd5a6af3a33e4ffdf39bf88966d28
Jeganathan Swaminathan <mail2jegan@gmail.com>
2025-10-16 15:29:20 +0530

    Added Day4 lab exercises.

---
+++ Day4/python-with-mysql/app.py
@@ -0,0 +1,38 @@
+from flask import Flask, jsonify
+import pymysql
+import os
+
+app = Flask(__name__)
+
+DB_HOST = os.environ.get("MYSQL_HOST", "mysql")
+DB_USER = os.environ.get("MYSQL_USER", "root")
+DB_PASSWOrD = os.environ.get("MYSQL_PASSWORD", "root@123")
+DB_NAME = os.environ.get("MYSQL_DATABASE", "flaskdb")
+
+def getConnection():
+    return pymysql.connect(
+       host=DB_HOST,
+       user=DB_USER,
+       password=DB_PASSWORD,
+       database=DB_NAME,
+       cursorclass=pymysql.cursors.DictCursor
+    )
+
+def index():
+    conn = getConnection)()
+    with conn.cursor() as cursor:
+        cursor.execute("CREATE TABLE IF NOT EXISTS visits (count INT)")
+        cursor.execute("SELECT count FROM visits")
+        result = cursor.fetchone()
+        if result is None:
+            cursor.execute("INSERT INTO visits (count) VALUES (1)")
+            visits = 1
+        else:
+            visits = result['count'] + 1
+            cursor.execute("UPDATE visits SET count=%s",(visits,))
+        conn.commit()
+    conn.close()
+    return jsonify({"message": f"Hello! This page has been visited {visits} times."})
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0",port=8080)
