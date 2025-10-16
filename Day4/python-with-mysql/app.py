30e28a90132c80c1816340bbb4d80fa931bfa2eb
Jeganathan Swaminathan <mail2jegan@gmail.com>
2025-10-16 16:31:28 +0530

    Fixed Dockerfile error

--- Day4/python-with-mysql/app.py
+++ Day4/python-with-mysql/app.py
@@ -5,8 +5,8 @@ import os
 app = Flask(__name__)
 
 DB_HOST = os.environ.get("MYSQL_HOST", "mysql")
-DB_USER = os.environ.get("MYSQL_USER", "root")
-DB_PASSWORD = os.environ.get("MYSQL_PASSWORD", "root@123")
+DB_USER = os.environ.get("MYSQL_USER", "flaskuser")
+DB_PASSWORD = os.environ.get("MYSQL_PASSWORD", "flaskpass")
 DB_NAME = os.environ.get("MYSQL_DATABASE", "flaskdb")
 
 def getConnection():
